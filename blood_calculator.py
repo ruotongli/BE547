1624d759d02eaae4ae6cb00ffc3f92660e6b54bf
Ruotong Li <ruotong.li@duke.edu>
2022-09-05 12:46:29 -0400

    Wrote function to receive input for HDL

--- blood_calculator.py
+++ blood_calculator.py
@@ -4,8 +4,13 @@ def interface():
     print('9 - Quit')
     keep_running=True
     while keep_running:
-        choice = input('Enter choices:')
+        choice = input('Enter choice:')
         if choice =='9':
             return
+
+def input_HDL():
+    HDL_input = input('Enter the HDL value:')
+    return int(HDL_input)
+        
 
 interface()
