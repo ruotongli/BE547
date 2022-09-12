3da5030beab0493eba3c2b3c564c2e7b2c23589c
Ruotong Li <ruotong.li@duke.edu>
2022-09-12 13:03:41 -0400

    Separated the code into easier to read module

--- dosing.py
+++ dosing.py
@@ -6,7 +6,7 @@
 """
 
 
-def dose_amount():
+def get_input():
     print("Day One Dosing Guidelines")
     print("")
     print("Choose diagnosis:")
@@ -19,19 +19,33 @@ def dose_amount():
     print("Enter patient weight followed by units of kg or lb.")
     print("Examples:  65.3 lb      21.0 kg")
     weight_input = input("Enter weight: ")
+    return diagnosis, weight_input
+
+def parse_input(weight_input)
     weight_data = weight_input.split(" ")
     weight = float(weight_data[0])
     units = weight_data[1]
+    return weight,units
+
+def calculate_dosage(diagnosis,weight,units)
     if units == "lb":
         weight = weight / 2.205
     dosages_mg_per_kg = [30, 10, 10, 12]
     dosage_mg_per_kg = dosages_mg_per_kg[diagnosis-1]
     dosage_mg_first_day = weight * dosage_mg_per_kg
+    return weight,dosage_mg_first_day
+
+def output_dose(weight,dosage_mg_first_day):
     print("CORRECT DOSAGE")
     print("For a patient weighing {:.1f} kg,".format(weight))
     print("  the correct dosage is {:.1f} mg the first day"
           .format(dosage_mg_first_day))
 
+def dose_amount():
+    diagnosis, weight_input = get_input()
+    weight,units = parse_input(weight_input)
+    weight,dosage_mg_first_day = calculate_dosage(diagnosis,weight,units)
+    output_dose(weight,dosage_mg_first_day)
 
 if __name__ == '__main__':
     dose_amount()
