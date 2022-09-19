6ff9bd99e37462dc86963a2a1155c6b31bbd9921
Ruotong Li <ruotong.li@duke.edu>
2022-09-19 13:02:50 -0400

    Added the test function for LDL and total cholesterol

--- test_blood_calculator.py
+++ test_blood_calculator.py
@@ -5,7 +5,24 @@ def test_check_HDL(input,expected):
     answer = check_HDL(input)
     assert answer == expected
     
+@pytest.mark.parametrize('input,expected',
+    [(120,'Normal'),
+    (140, 'Borderline high'),
+    (170,'High'),
+    (200,'Very high')])
+def test_check_LDL(input,expected):
+    from blood_calculator import check_LDL
+    answer = check_LDL(input)
+    assert answer == expected
 
+@pytest.mark.parametrize('input,expected',
+    [(180,'Normal'),
+    (220, 'Borderline high'),
+    (250,'High')])
+def test_check_total(input,expected):
+    from blood_calculator import check_total
+    answer = check_total(input)
+    assert answer == expected
 """
 def test_check_HDL_Borderlinelow(input,expected):
     from blood_calculator import check_HDL
