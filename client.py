487b54f5f5599d54827320c5a9917de9f5fb8195
Ruotong Li <ruotong.li@duke.edu>
2022-10-19 13:16:25 -0400

    Server

---
+++ client.py
@@ -0,0 +1,18 @@
+import requests
+
+#r = requests.get('http://127.0.0.1:5000/info')
+#print(r.status_code)
+#print(r.text)
+'''
+out_data = {'name': 'Ruotong Li',
+            'hdl_value': 150}
+r = requests.post('http://127.0.0.1:5000/hdl_check', json=out_data)
+print(r.status_code)
+print(r.text)
+'''
+out_data = {'a': 5,
+            'b': 12}
+r = requests.post('http://127.0.0.1:5000/addition', json=out_data)
+print(r.status_code)
+print(r.text)
+answer = r.json()
---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def server_status():
+    return 'Server is on.'
+
+@app.route('/info', methods=['GET'])
+def information():
+    x = 'This website will calculate blood cholesterol levels.\n'
+    x += 'It is written by Ward.'
+    return x
+
+@app.route('/hdl_check', methods=['POST'])
+def hdl_check_from_internet():
+    '''
+        incoming_json = {'name': <name_str>,
+                         'hdl_value': <hdl_value_int>}
+    '''
+    from blood_calculator import check_HDL
+    in_data = request.get_json()
+    hdl_value = in_data['hdl_value']
+    answer = check_HDL(hdl_value)
+    return answer
+
+@app.route('/addition', methods=['POST'])
+def add_numbers():
+    in_data = request.get_json()
+    answer = in_data['a'] + in_data['b']
+    return jsonify(answer)
+
+
+if __name__ == '__main__':
+    app.run()
